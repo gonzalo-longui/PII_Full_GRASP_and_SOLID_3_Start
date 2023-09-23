f5c870c9a0017959d89d905b8f7679c7b0e5a55d
longuigon <longuigon+git@gmail.com>
2023-09-23 15:59:28 -0300

    Final

--- src/Library/ConsolePrinter.cs
+++ src/Library/ConsolePrinter.cs
@@ -5,6 +5,11 @@ namespace Full_GRASP_And_SOLID.Library
 {
     public class ConsolePrinter : IPrinter
     {
+        /*
+        Principio SRP: Esta clase tiene la única responsabilidad de imprimir la receta en la consola
+        Polymorphism: Esta clase y la clase FilePrinter implementan la interfaz IPrinter. Tienen el método PrintRecipe, que 
+        se implementa de manera distinta dependiendo de la clase, pero que se invoca de la misma manera en el Program.
+        */
         public void PrintRecipe(Recipe recipe)
         {
             Console.WriteLine(recipe.GetTextToPrint());
--- src/Library/FilePrinter.cs
+++ src/Library/FilePrinter.cs
@@ -5,6 +5,11 @@ namespace Full_GRASP_And_SOLID.Library
 {
     public class FilePrinter : IPrinter
     {
+        /*
+        Principio SRP: Esta clase tiene la única responsabilidad de imprimir la receta en un archivo
+        Polymorphism: Esta clase y la clase ConsolePrinter implementan la interfaz IPrinter. Tienen el método PrintRecipe, que 
+        se implementa de manera distinta dependiendo de la clase, pero que se invoca de la misma manera en el Program.
+        */
         public void PrintRecipe(Recipe recipe)
         {
             File.WriteAllText("Recipe.txt", recipe.GetTextToPrint());
